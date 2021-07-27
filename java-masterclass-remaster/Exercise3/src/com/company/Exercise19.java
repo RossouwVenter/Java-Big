620ecb17e3162ad523566df53dee3c9e45abded6
Rossouw <rossouw.venter.rv@gmail.com>
2021-07-27 12:23:19 +0200

    Exercise 19

--- java-masterclass-remaster/Exercise3/src/com/company/Exercise19.java
+++ java-masterclass-remaster/Exercise3/src/com/company/Exercise19.java
@@ -3,6 +3,30 @@ package com.company;
 public class Exercise19 {
 
     public static boolean hasSameLastDigit(int parameterOne,int parameterTwo,int parameterThree){
-        if ((parameterOne <= 10 || parameterOne >= 1000) && (parameterTwo <= 10 || parameterTwo >= 1000) && (parameterThree <= 10 || parameterThree >= 1000))
+
+        if ((parameterOne <= 10 || parameterOne >= 1000) && (parameterTwo <= 10 || parameterTwo >= 1000)
+                && (parameterThree <= 10 || parameterThree >= 1000)) {
+            int lastDigitFirst = parameterOne % 10;
+            int lastDigitSecond = parameterTwo % 10;
+            int lastDigitThird = parameterThree % 10;
+
+            if ((lastDigitFirst == lastDigitSecond) || (lastDigitFirst == lastDigitThird) || (lastDigitSecond == lastDigitThird)) {
+                return true;
+            } else {
+                return false;
+            }
+
+        } else {
+            return false;
+        }
     }
+
+    public static boolean isValid(int parameter){
+        if(parameter >= 10 && parameter <= 1000){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
 }
