b36a5e11488b241ce39d29d241b80dd0f54fffe1
Rossouw <rossouw.venter.rv@gmail.com>
2021-07-28 14:40:53 +0200

    ReaderUserInputChallenge complete

--- java-masterclass-remaster/ReadingUserInput/src/com/company/ReaderUserInputChallenge.java
+++ java-masterclass-remaster/ReadingUserInput/src/com/company/ReaderUserInputChallenge.java
@@ -1,4 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class ReaderUserInputChallenge {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = 1;
+        while (number >= 1){
+            System.out.println("Enter the number #" + number);
+            int inputNum = scanner.nextInt();
+            if (inputNum == number){
+                scanner.nextLine();
+                number++;
+            } else {
+                System.out.println("Not the same number!");
+            }
+
+        }
+
+        scanner.close();
+    }
 }
