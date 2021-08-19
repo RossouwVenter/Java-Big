8722029d8a3c9366d1c580b79da57ed740730a7e
Rossouw <rossouw.venter.rv@gmail.com>
2021-08-19 15:18:34 +0200

    Class Example

---
+++ java-masterclass-remaster/Class_Example/src/com/company/_C.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class _C {
+    private static int $;
+
+    public static void main(String[] main) {
+        String a_b = "Hello World";
+        System.out.print($);
+        System.out.print(a_b);
+    }
+}
