92b17396c2f7f944a1b00f70e65d61be4563d847
Rossouw <rossouw.venter.rv@gmail.com>
2021-08-23 11:52:33 +0200

    Inheritance Challenge

---
+++ java-masterclass-remaster/Inheritance_Challenge/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	BMW suv = new BMW(36);
+	suv.steer(45);
+	suv.accelerate(30);
+	suv.accelerate(20);
+    }
+}
