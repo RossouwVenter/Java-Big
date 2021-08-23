8afeec3a571013dd18ca4843e9ccdceb6f5d1f43
Rossouw <rossouw.venter.rv@gmail.com>
2021-08-23 10:20:24 +0200

    classes exercise 34

--- java-masterclass-remaster/Class_Example/src/com/company/_C.java
+++ java-masterclass-remaster/Class_Example/src/com/company/_C.java
@@ -2,9 +2,11 @@ package com.company;
 
 public class _C {
     private static int $;
+//    private static String a_b;
 
     public static void main(String[] main) {
-        String a_b = "Hello World";
+         String a_b = "abc";
+
         System.out.print($);
         System.out.print(a_b);
     }
