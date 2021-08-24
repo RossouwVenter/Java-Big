874017851bc11095575763584b3009df16bf5cba
Rossouw <rossouw.venter.rv@gmail.com>
2021-08-24 08:51:31 +0200

    Oracle Exam Questions 5-7

---
+++ java-masterclass-remaster/OracleExam_prep/src/com/company/Question5.java
@@ -0,0 +1,21 @@
+package com.company;
+
+//public class Question5 {
+//    System.out.print("a");
+//        try
+//
+//    {
+//        System.out.print("b");
+//        throw new IllegalArgumentException();
+//    } catch(
+//    RuntimeException e)
+//
+//    {
+//        System.out.print("c");
+//    } finally
+//
+//    {
+//        System.out.print("d");
+//    }
+//System.out.print("e");
+//}
---
+++ java-masterclass-remaster/OracleExam_prep/src/com/company/Question6.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Question6 {
+//    public class MathFunctions {
+        public static void addToInt(int x, int amountToAdd) {
+            x = x + amountToAdd;
+        }
+
+        public static void main(String[] args) {
+            int a = 15;
+            int b = 10;
+//            MathFunctions.addToInt(a, b);
+            Question6.addToInt(a, b);
+            System.out.println(a);
+        }
+    }
+}
