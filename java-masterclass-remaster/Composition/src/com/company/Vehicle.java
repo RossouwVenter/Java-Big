d358c4923c0c421ebad031c6083ff40f307874c2
Rossouw <rossouw.venter.rv@gmail.com>
2021-08-24 08:21:18 +0200

    Composition

---
+++ java-masterclass-remaster/Composition/src/com/company/Vehicle.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Vehicle {
+    private int doors;
+    private int engineCapacity;
+
+    public Vehicle(int doors, int engineCapacity) {
+        this.doors = doors;
+        this.engineCapacity = engineCapacity;
+    }
+}
