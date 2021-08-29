7c40a98b8aad7904b98da840239d561771b5e395
Rossouw <rossouw.venter.rv@gmail.com>
2021-08-29 11:44:11 +0200

    Arrays Challenge

---
+++ java-masterclass-remaster/OracleExam_prep/src/com/company/Grasshopper.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.*;
+
+public class Grasshopper {
+
+    public  Grasshopper(String n) {
+
+            name = n;
+
+
+
+    }
+    public static void main(String[] args) {
+        Grasshopper one = new Grasshopper("g1");
+        Grasshopper two = new Grasshopper("g2");
+        one = two;
+        two = null;
+        one = null;
+    }
+
+    private String name;
+}
