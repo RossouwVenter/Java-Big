19fc3f991570b5eefac1aa14c98eeb6d1a9f322e
Rossouw Venter <77490024+RossouwVenter@users.noreply.github.com>
2021-09-03 08:10:12 +0200

    Autoboxing Challenge part 3 done

--- java-masterclass-remaster/Autoboxing_Unboxing_Challenge/src/com/company/Main.java
+++ java-masterclass-remaster/Autoboxing_Unboxing_Challenge/src/com/company/Main.java
@@ -11,16 +11,31 @@ public class Main {
         bank.addCustomer("Adelaide", "Percy", 220.12);
 
         bank.addBranch("Sydney");
-        bank.addCustomer("Sydney","Bob",150.54);
+        bank.addCustomer("Sydney", "Bob", 150.54);
 
-        bank.addCustomerTransaction("Adelaide","Tim",44.22);
-        bank.addCustomerTransaction("Adelaide","Tim",12.44);
-        bank.addCustomerTransaction("Adelaide","Mike",1.55);
+        bank.addCustomerTransaction("Adelaide", "Tim", 44.22);
+        bank.addCustomerTransaction("Adelaide", "Tim", 12.44);
+        bank.addCustomerTransaction("Adelaide", "Mike", 1.55);
 
         bank.listCustomers("Adelaide", false);
         bank.listCustomers("Sydney", true);
 
-//        if(!bank.addCustomer("Melbourne"));
+        bank.addBranch("Melbourne");
+        if (!bank.addCustomer("Melbourne", "Brian", 5.55)) {
+            System.out.println("ErrorMelbourne branch does not exist");
+        }
+
+        if (!bank.addBranch("Adelaide")) {
+            System.out.println("Adelaide already exists");
+        }
+
+        if (!bank.addCustomerTransaction("Adelaide", "Fergus",52.13)){
+            System.out.println("Customer does not exist at branch");
+        }
+
+        if (!bank.addCustomer("Adelaide","Tim",12.21)){
+            System.out.println("Customer Tim already exists");
+        }
 
     }
 }
