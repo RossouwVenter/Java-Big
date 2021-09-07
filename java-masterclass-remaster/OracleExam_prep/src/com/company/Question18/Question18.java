b45ec633be0446c81468cfb5b50601f85d00cbe0
Rossouw Venter <77490024+RossouwVenter@users.noreply.github.com>
2021-09-07 09:44:33 +0200

    Question18 Oracle Exam

---
+++ java-masterclass-remaster/OracleExam_prep/src/com/company/Question18/Question18.java
@@ -0,0 +1,16 @@
+package com.company.Question18;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Calendar;
+
+public class Question18 {
+    public static void main(String[] args) {
+//         System.out.println(LocalDate.of(2015, Calendar.APRIL, 1));
+         System.out.println(LocalDate.of(2015, Month.APRIL, 1));
+         System.out.println(LocalDate.of(2015, 3, 1));
+//         System.out.println(LocalDate.of(2015, 4, 1));
+//         System.out.println(new LocalDate(2015, 3, 1));
+//         System.out.println(new LocalDate(2015, 4, 1));
+    }
+}
