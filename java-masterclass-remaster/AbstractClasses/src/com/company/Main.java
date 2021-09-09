06f65f6de12ccae7b8265c6e83afccaee70f6412
Rossouw Venter <77490024+RossouwVenter@users.noreply.github.com>
2021-09-09 11:37:02 +0200

    Abstract classes part 1

---
+++ java-masterclass-remaster/AbstractClasses/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 Dog dog = new Dog("Yorkie");
+	 dog.breathe();
+	 dog.eat();
+
+	 Parrot bird = new Parrot("Australian ring neck");
+	 bird.breathe();
+	 bird.eat();
+	 bird.fly();
+
+	 Penguin penguin = new Penguin("Penquin");
+	 penguin.fly();
+    }
+}
