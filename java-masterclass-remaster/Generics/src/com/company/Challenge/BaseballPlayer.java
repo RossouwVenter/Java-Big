6ee505ba5fd208a271b6a5c338c1cd4575350496
Rossouw Venter <77490024+RossouwVenter@users.noreply.github.com>
2021-09-13 12:47:54 +0200

    Generics challenge

---
+++ java-masterclass-remaster/Generics/src/com/company/Challenge/BaseballPlayer.java
@@ -0,0 +1,9 @@
+package com.company.Challenge;
+
+//import com.company.Generics_Class.Player;
+
+public class BaseballPlayer extends Player {
+    public BaseballPlayer(String name) {
+        super(name);
+    }
+}
---
+++ java-masterclass-remaster/Generics/src/com/company/Challenge/FootballPlayer.java
@@ -0,0 +1,9 @@
+package com.company.Challenge;
+
+//import com.company.Generics_Class.Player;
+
+public class FootballPlayer extends Player {
+    public FootballPlayer(String name) {
+        super(name);
+    }
+}
