b07576c56999bf3ecb54b43b0450817a535b9fab
Rossouw Venter <77490024+RossouwVenter@users.noreply.github.com>
2021-09-14 09:22:15 +0200

    Window

---
+++ java-masterclass-remaster/Naming Conventions/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import org.w3c.dom.Node;
+//import javafx.scene.Node;
+
+import org.w3c.dom.Node;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        javafx.scene.Node node = null;
+        Node node = null;
+	    org.w3c.dom.Node anotherNode = null;
+    }
+}
