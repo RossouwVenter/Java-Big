ed006d24de8dcc8d9ffa8d39af6986ed573e5fed
Rossouw Venter <77490024+RossouwVenter@users.noreply.github.com>
2021-09-14 12:24:02 +0200

    Scope part 2

--- java-masterclass-remaster/Scope/src/com/company/mypackage/ScopeCheck.java
+++ java-masterclass-remaster/Scope/src/com/company/mypackage/ScopeCheck.java
@@ -2,20 +2,46 @@ package com.company.mypackage;
 
 public class ScopeCheck {
     public int publicVar = 0;
-    private int privateVar = 1;
+    private int var1 = 1;
 
     public ScopeCheck(){
-        System.out.println("ScopeCheck created, publicVar = " + publicVar + ": privateVar= " + privateVar);
+        System.out.println("ScopeCheck created, publicOne = " + publicVar + ": privateVar = " + var1);
     }
 
-    public int getPrivateVar() {
-        return privateVar;
+    public int getVar1() {
+        return var1;
     }
 
     public void timesTwo(){
-        int privateVar = 2;
+        int varTwo = 2;
         for(int i =0;i<10;i++){
-            System.out.println(i + "times two is " + i * privateVar);
+            System.out.println(i + " times two is " + i * varTwo);
         }
+    }
+
+    public void useInner(){
+        InnerClass innerClass = new InnerClass();
+        System.out.println("varThree from outer clas: " + var1);
+    }
+
+    public class InnerClass{
+        private int var3 = 3;
+
+        public InnerClass(int privateVar) {
+            this.var3 = privateVar;
+        }
+
+        public InnerClass(){
+            System.out.println("Inner class created,privateVar is " + var3);
+        }
+
+        public void timesTwo(){
+            ScopeCheck.this.timesTwo();
+//            int privateVar = 2;
+            for(int i =0;i<10;i++){
+                System.out.println(i + " times two is " + i * var3);
+            }
+        }
+
     }
 }
