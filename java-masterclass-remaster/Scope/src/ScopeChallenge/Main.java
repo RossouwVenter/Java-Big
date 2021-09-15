34c0c2a5a0047855ba1d87a5223c564dd270843e
Rossouw Venter <77490024+RossouwVenter@users.noreply.github.com>
2021-09-15 09:02:55 +0200

    Scope Challenge

---
+++ java-masterclass-remaster/Scope/src/ScopeChallenge/Main.java
@@ -0,0 +1,14 @@
+package ScopeChallenge;
+
+import java.util.Scanner;
+// Driver Rossouw Venter
+// Navigators: James Stromnes, Muhammed Gardee;
+
+public class Main {
+
+    public static void main(String[] args) {
+        X x = new X(new Scanner(System.in));
+
+        x.x();
+    }
+}
