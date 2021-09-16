cc0cf28c94e76f74efd9548e0f0a475aa56c650c
Rossouw Venter <77490024+RossouwVenter@users.noreply.github.com>
2021-09-16 09:50:05 +0200

    Maps

---
+++ java-masterclass-remaster/Collections/src/com/company/mypackage/MapProgram.java
@@ -0,0 +1,40 @@
+package com.company.mypackage;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapProgram {
+
+    public static void main(String[] args) {
+//        Map<String, String> languages = new HashMap<>();
+        Map<String, String> languages = new HashMap<>();
+
+        if(languages.containsKey("Java")){
+            System.out.println("Java is already in the map");
+        } else{
+            languages.put("Java", "This course is about Java");
+            System.out.println("Java added successfully");
+        }
+        languages.put("Java","a compiled high level,...");
+        languages.put("Python","an interpreted,...");
+        languages.put("Algol", "an algorithmic language");
+        System.out.println(languages.put("BASIC", "Beginners All Purpose Symbolic Instruction Code"));
+        System.out.println(languages.put("Lisp","Therein lies madness"));
+
+
+        if(languages.containsKey("Java")){
+            System.out.println("Java is already in the map");
+        } else{
+            languages.put("Java", "This course is about Java");
+        }
+////        System.out.println(languages.get("Java"));
+//        languages.put("Java", "this course about java");
+//        System.out.println(languages.get("Java"));
+
+        System.out.println("=======================================");
+
+        for(String key: languages.keySet()){
+            System.out.println(key + " : " + languages.get(key));
+        }
+    }
+}
