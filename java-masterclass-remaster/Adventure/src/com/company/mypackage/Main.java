7670bb1a17d8544b1306376c73c86d16099275fd
Rossouw Venter <77490024+RossouwVenter@users.noreply.github.com>
2021-09-16 11:17:29 +0200

    Adventure Game Challenge

--- java-masterclass-remaster/Adventure/src/com/company/mypackage/Main.java
+++ java-masterclass-remaster/Adventure/src/com/company/mypackage/Main.java
@@ -1,12 +1,14 @@
 package com.company.mypackage;
 
+//Driver: James Stromnes;
+//Navigators: Muhammed Gardee & Rossouw Venter;
+
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-    private static Map<Integer,Location> locations = new HashMap<>();
+    private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -18,55 +20,65 @@ public class Main {
         locations.put(4, new Location(4, "You are in a valley beside a stream"));
         locations.put(5, new Location(5, "You are in the forest"));
 
-        locations.get(1).addExit("W",2);
-        locations.get(1).addExit("E",3);
-        locations.get(1).addExit("S",4);
-        locations.get(1).addExit("N",5);
-//        locations.get(1).addExit("Q",0);
+        locations.get(1).addExit("W", 2);
+        locations.get(1).addExit("E", 3);
+        locations.get(1).addExit("S", 4);
+        locations.get(1).addExit("N", 5);
 
-        locations.get(2).addExit("N",5);
-//        locations.get(2).addExit("Q",0);
 
-        locations.get(3).addExit("W",1);
-//        locations.get(3).addExit("Q",0);
+        locations.get(2).addExit("N", 5);
 
-        locations.get(1).addExit("N",1);
-        locations.get(1).addExit("W",2);
-//        locations.get(1).addExit("Q",0);
 
-        locations.get(1).addExit("S",1);
-        locations.get(1).addExit("W",2);
-//        locations.get(1).addExit("Q",0);
+        locations.get(3).addExit("W", 1);
+
+
+        locations.get(4).addExit("N", 1);
+        locations.get(4).addExit("W", 2);
+
+
+        locations.get(5).addExit("S", 1);
+        locations.get(5).addExit("W", 2);
+
+        Map<String, String> vocabulary = new HashMap<String, String>();
+        vocabulary.put("NORTH", "N");
+        vocabulary.put("SOUTH", "S");
+        vocabulary.put("EAST", "E");
+        vocabulary.put("WEST", "W");
+        vocabulary.put("QUIT", "Q");
 
 
         int loc = 1;
-        while(true) {
+        while (true) {
             System.out.println(locations.get(loc).getDescription());
-            if(loc == 0) {
+            if (loc == 0) {
                 break;
             }
 
-            Map<String,Integer> exists = locations.get(loc).getExists();
-            System.out.println("Available exists are");
-            for (String exit: exists.keySet()) {
-                System.out.println(exit + ", ");
+            Map<String, Integer> exits = locations.get(loc).getExists();
+            System.out.print("Available exits are ");
+            for (String exit : exits.keySet()) {
+                System.out.print(exit + ", ");
             }
             System.out.println();
 
-
-
             String direction = scanner.nextLine().toUpperCase();
-
-            if(exists.containsKey(direction)){
-                loc = exists.get(direction);
-            } else {
-                System.out.println("You cannot go in that direction");
+            if(direction.length() > 1) {
+                String[] words = direction.split(" ");
+                for(String word : words) {
+                    if(vocabulary.containsKey(word)) {
+                        direction = vocabulary.get(word);
+                        break;
+                    }
+                }
             }
 
-            loc = scanner.nextInt();
-            if(!locations.containsKey(loc)) {
+            if (exits.containsKey(direction)) {
+                loc = exits.get(direction);
+
+            } else {
                 System.out.println("You cannot go in that direction");
             }
         }
     }
 }
+
