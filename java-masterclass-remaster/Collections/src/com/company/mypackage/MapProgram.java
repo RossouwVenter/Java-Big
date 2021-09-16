b07cd5c15fef48278c78390c2d6fca8662b1dec2
Rossouw Venter <77490024+RossouwVenter@users.noreply.github.com>
2021-09-16 10:31:12 +0200

    Adventure Game

--- java-masterclass-remaster/Collections/src/com/company/mypackage/MapProgram.java
+++ java-masterclass-remaster/Collections/src/com/company/mypackage/MapProgram.java
@@ -15,6 +15,7 @@ public class MapProgram {
             languages.put("Java", "This course is about Java");
             System.out.println("Java added successfully");
         }
+
         languages.put("Java","a compiled high level,...");
         languages.put("Python","an interpreted,...");
         languages.put("Algol", "an algorithmic language");
@@ -27,12 +28,28 @@ public class MapProgram {
         } else{
             languages.put("Java", "This course is about Java");
         }
-////        System.out.println(languages.get("Java"));
+//        System.out.println(languages.get("Java"));
 //        languages.put("Java", "this course about java");
 //        System.out.println(languages.get("Java"));
 
         System.out.println("=======================================");
 
+//         Ways to remove in map
+
+//        languages.remove("Lisp");
+
+        if(languages.remove("Algol","an algorithmic languages")){
+            System.out.println("Algol removed");
+        }else {
+            System.out.println("Algol not removed, key/value pair not found");
+        }
+
+        if (languages.replace("Lisp","This will not work","a functional programming language with imperial features")) {
+            System.out.println("Lisp replaced");
+        } else {
+            System.out.println("Lisp was not replaced");
+        }
+        System.out.println(languages.replace("Scala", "this will not be added"));
         for(String key: languages.keySet()){
             System.out.println(key + " : " + languages.get(key));
         }
