7b77b473c893e128d14493d60a42946bc04e897c
Rossouw Venter <77490024+RossouwVenter@users.noreply.github.com>
2021-10-07 08:41:57 +0200

    Add DialogPane

--- java-masterclass-remaster/TodoList/src/main/java/com/rossouw/todolist/Controller.java
+++ java-masterclass-remaster/TodoList/src/main/java/com/rossouw/todolist/Controller.java
@@ -5,15 +5,13 @@ import com.rossouw.todolist.datamodel.TodoItem;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.TextArea;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
 
-import java.time.LocalDate;
-import java.time.Month;
+import java.io.IOException;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Controller {
@@ -25,6 +23,8 @@ public class Controller {
     private TextArea itemDetailsTextArea;
     @FXML
     private Label deadlineLabel;
+    @FXML
+    private BorderPane mainBorderPane;
 
     public void initialize(){
 //        TodoItem item1 = new TodoItem("Mail birthday card","Buy a 30th birthday card for Jhon",
@@ -47,9 +47,11 @@ public class Controller {
 //
 //        TodoData.getInstance().setTodoItems(todoItems);
 
+
+
         todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TodoItem>() {
             @Override
-            public void changed(ObservableValue<? extends TodoItem> observable TodoItem oldValue, TodoItem newValue) {
+            public void changed(ObservableValue<? extends TodoItem> observable, TodoItem oldValue, TodoItem newValue) {
                if(newValue != null){
                    TodoItem item = todoListView.getSelectionModel().getSelectedItem();
                    itemDetailsTextArea.setText(item.getDetails());
@@ -63,6 +65,22 @@ public class Controller {
         todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         todoListView.getSelectionModel().selectFirst();
     }
+
+    @FXML
+    public void showNewItemDialog(){
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(mainBorderPane.getScene().getWindow());
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("todoItemDialog.fxml"));
+            dialog.getDialogPane().setContent(root);
+        } catch (IOException e){
+            System.out.println("Couldn't load the dialog");
+            e.printStackTrace();
+            return;
+        }
+
+    }
+
    @FXML
     public void handleClickListView(){
         TodoItem item =  todoListView.getSelectionModel().getSelectedItem();
---
+++ java-masterclass-remaster/TodoList/src/main/java/com/rossouw/todolist/DialogController.java
@@ -0,0 +1,4 @@
+package com.rossouw.todolist;
+
+public class DialogController {
+}
