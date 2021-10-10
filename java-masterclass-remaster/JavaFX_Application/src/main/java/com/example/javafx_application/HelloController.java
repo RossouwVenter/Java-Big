8866e06669b990e3fe32211ee75a824b02fe13ce
Rossouw Venter <77490024+RossouwVenter@users.noreply.github.com>
2021-10-10 15:47:09 +0200

    Nodes and Choosers

--- java-masterclass-remaster/JavaFX_Application/src/main/java/com/example/javafx_application/HelloController.java
+++ java-masterclass-remaster/JavaFX_Application/src/main/java/com/example/javafx_application/HelloController.java
@@ -1,14 +1,61 @@
 package com.example.javafx_application;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.GridPane;
+import javafx.stage.DirectoryChooser;
+
+import java.io.File;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private Button button4;
+
+    @FXML
+    private GridPane gridPane;
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    private Label label;
+
+    public void initialize(){
+
+        button4.setEffect(new DropShadow());
+//        label.setScaleX(2.0);
+//        label.setScaleY(2.0);
+    }
+    @FXML
+    public void handleMouseEnter(){
+        label.setScaleX(2.0);
+        label.setScaleY(2.0);
+    }
+
+    @FXML
+    public void handleMouseExit(){
+        label.setScaleX(1.0);
+        label.setScaleY(1.0);
+    }
+    @FXML
+    public void handleClick(){
+//        FileChooser chooser = new FileChooser();
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.showDialog(gridPane.getScene().getWindow());
+        File file = chooser.showDialog(gridPane.getScene().getWindow());
+        if(file != null){
+            System.out.println(file.getPath());
+        } else {
+            System.out.println("Chooser was canceled");
+        }
+//        chooser.showOpenDialog(gridPane.getScene().getWindow());
+
     }
 }
