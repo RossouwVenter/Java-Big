70352d3844601b65b18c10caaf912be330aebb6d
Rossouw Venter <77490024+RossouwVenter@users.noreply.github.com>
2021-10-10 19:24:12 +0200

    More Choosers and WebPages

--- java-masterclass-remaster/JavaFX_Application/src/main/java/com/example/javafx_application/HelloController.java
+++ java-masterclass-remaster/JavaFX_Application/src/main/java/com/example/javafx_application/HelloController.java
@@ -5,9 +5,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.GridPane;
-import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
 
 public class HelloController {
     @FXML
@@ -18,6 +22,10 @@ public class HelloController {
 
     @FXML
     private GridPane gridPane;
+
+    @FXML
+    private WebView webView;
+
 
     @FXML
     protected void onHelloButtonClick() {
@@ -46,16 +54,41 @@ public class HelloController {
     }
     @FXML
     public void handleClick(){
-//        FileChooser chooser = new FileChooser();
-        DirectoryChooser chooser = new DirectoryChooser();
-        chooser.showDialog(gridPane.getScene().getWindow());
-        File file = chooser.showDialog(gridPane.getScene().getWindow());
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Save Application File");
+        chooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Zip", "*.zip"),
+                new FileChooser.ExtensionFilter("PDF","*.pdf"),
+                new FileChooser.ExtensionFilter("Image Files", "*.jpeg","*.jpg"),
+                new FileChooser.ExtensionFilter("All Files","*.*")
+        );
+//        DirectoryChooser chooser = new DirectoryChooser();
+//        chooser.showDialog(gridPane.getScene().getWindow());
+        List<File> file = chooser.showOpenMultipleDialog(gridPane.getScene().getWindow());
         if(file != null){
-            System.out.println(file.getPath());
+            for(int i=0; i< file.size();i++)
+            System.out.println(file.get(i));
         } else {
             System.out.println("Chooser was canceled");
         }
 //        chooser.showOpenDialog(gridPane.getScene().getWindow());
 
     }
+
+
+    @FXML
+    public void handleLinkClick(){
+
+        WebEngine engine = webView.getEngine();
+        engine.load("http://www.javafx.com");
+//      try{
+//          Desktop.getDesktop().browse(new URI("http://www.javafx.com"));
+//      } catch (IOException e){
+//          e.printStackTrace();
+//      }catch (URISyntaxException e) {
+//          e.printStackTrace();
+//      }
+    }
+
+
 }
