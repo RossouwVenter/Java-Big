bdbdd593fd92f7257f74ad17462bd38285712516
Rossouw Venter <77490024+RossouwVenter@users.noreply.github.com>
2021-10-11 08:16:53 +0200

    Scene Builder part 2

--- java-masterclass-remaster/SceneBuilder/src/main/java/com/example/scenebuilder/HelloController.java
+++ java-masterclass-remaster/SceneBuilder/src/main/java/com/example/scenebuilder/HelloController.java
@@ -4,11 +4,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+   @FXML
+    private Label label;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+   @FXML
+    public  void handleAction(){
+       label.setText("OK button pressed");
+   }
 }
